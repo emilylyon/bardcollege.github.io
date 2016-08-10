d99740cd1b0421758429a435749c2b2dffe3fd7e
emilylyon <emilybliss63@gmail.com>
2016-08-10 15:05:11 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -40,8 +40,11 @@ var rules = {
     "What do you think about computers?",
     "What do you think machines have to do with your problem?",
   ],
+  "~*x emily ~*y": [
+  	"hi emily"
+  ],
   "~*x name ~*y": [
-    "I am not interested in names",
+    "I am not interested in names."
   ],
   "~*x sorry ~*y": [
     "Please don't apologize",
